49f55dc07513294d454a46121950ca6befd9c444
Ben Houston <neuralsoft@gmail.com>
2023-06-19 07:05:38 -0400

    simplify tsconfig

--- apps/cmdline-app/src/index.ts
+++ apps/cmdline-app/src/index.ts
@@ -1,5 +1,8 @@
 import { getHelloWorld } from '@esbuild-ts-monorepo/vanilla-lib';
+import { name, version } from '../package.json';
 
 export const main = async () => {
+  console.log(`${name} v${version}`);
+
   console.log(getHelloWorld());
 };
