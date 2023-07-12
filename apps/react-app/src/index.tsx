3eead0647e303713b8422a0631810580817e82a0
Ben Houston <neuralsoft@gmail.com>
2023-07-12 10:05:26 -0400

    forgotten commit.

--- apps/react-app/src/index.tsx
+++ apps/react-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import App from './app.js';
+import App from './App.js';
 import { createRoot } from 'react-dom/client';
 
 const container = document.querySelector('#root');
