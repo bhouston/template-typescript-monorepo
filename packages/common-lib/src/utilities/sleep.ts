42bf9968046fa4e61f0633cb00cba655e672190e
Ben Houston <neuralsoft@gmail.com>
2025-02-13 13:19:02 -0500

    fix lint and docker file.

--- packages/common-lib/src/utilities/sleep.ts
+++ packages/common-lib/src/utilities/sleep.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-promise-executor-return */
 export function sleep(durationInMilliseconds: number) {
   return new Promise((resolve) =>
     setTimeout(resolve, Math.round(durationInMilliseconds)),
