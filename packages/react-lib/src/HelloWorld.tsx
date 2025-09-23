6972780ac50c337047d0bc00bd163e0bcfc30824
Ben Houston <neuralsoft@gmail.com>
2025-09-23 08:57:57 -0400

    slightly more informative message.

--- packages/react-lib/src/HelloWorld.tsx
+++ packages/react-lib/src/HelloWorld.tsx
@@ -11,7 +11,7 @@ export const HelloWorld: FC<HelloWorldProps> = ({ name, children }) => (
   <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100">
     <h1 className="text-6xl text-green-500 mb-6">Hello, {name}!</h1>
     <p className="text-xl text-black">
-      {toCamelCase('Welcome to our TailwindCSS + React app!')}
+      {toCamelCase('Welcome to Ben Houston\'s TypeScript Monorepo Template App!')}
     </p>
     {children}
   </div>
